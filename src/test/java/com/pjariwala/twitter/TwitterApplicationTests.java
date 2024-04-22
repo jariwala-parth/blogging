0225ba7ca59f7110492e862f84bafff48e9b169d
Parth Jariwala <parth.jariwala13@gmail.com>
2024-04-22 13:19:03 +0530

    Rename to Twitter

--- src/test/java/com/pjariwala/twitter/TwitterApplicationTests.java
+++ src/test/java/com/pjariwala/twitter/TwitterApplicationTests.java
@@ -1,10 +1,10 @@
-package com.pjariwala.blogging;
+package com.pjariwala.twitter;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BloggingApplicationTests {
+class TwitterApplicationTests {
 
 	@Test
 	void contextLoads() {
